30db1d54a050349aa548ddce7a21875ebc1c42bd
Daniels Jānis Rupenheits <Rogstrixs321@gmail.com>
2024-06-28 03:32:36 +0300

    33333

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-document.getElementById("glow-on-hover").addEventListener("click", function() {
-  var moreText = document.getElementById("moreText");
-  if (moreText.style.display === "none" || moreText.style.display === "") {
-    moreText.style.display = "block";
-    this.innerHTML = "Read Less";
-  } else {
-    moreText.style.display = "none";
-    this.innerHTML = "Read More";
-  }
+let moreButtons = document.querySelectorAll('.more-button');
+
+moreButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    let moreTextId = button.parentNode.querySelector('p').id;
+    let moreText = document.getElementById(moreTextId);
+    if (moreText.style.display === "none" || moreText.style.display === "") {
+      moreText.style.display = "block";
+      button.innerHTML = "Read Less";
+    } else {
+      moreText.style.display = "none";
+      button.innerHTML = "Read More";
+    }
+  });
 });
